e327c54fa8a0db0998b70d043e7da3fc294637f2
eswaribala <parameswaribala@gmail.com>
2021-08-10 15:06:36 +0530

    updated

--- day3/banking-app/src/Accounts/accounts.tsx
+++ day3/banking-app/src/Accounts/accounts.tsx
@@ -1,11 +1,78 @@
-import React from "react";
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Registration from "../Registration/registration";
+import Login from "../Login/login";
+import './account.css'
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
 
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
 
-export class Accounts extends React.Component<any, any>{
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
 
-    render(){
-        return(
-            <h1>Accounts</h1>
-        )
-    }
+function a11yProps(index: any) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function Accounts() {
+    //react hook
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className="accounts">
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab label="Registration" {...a11yProps(0)} />
+                    <Tab label="Login" {...a11yProps(1)} />
+
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                <Registration/>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+               <Login/>
+            </TabPanel>
+
+        </div>
+    );
 }
--- day3/banking-app/src/BankMenu/bankmenu.tsx
+++ day3/banking-app/src/BankMenu/bankmenu.tsx
@@ -3,12 +3,12 @@ import {TabMenu} from "primereact/tabmenu";
 import {Button} from "primereact/button";
 import {HashRouter,Switch,Route} from "react-router-dom";
 import {Home} from "../Home/home";
-import {Accounts} from "../Accounts/accounts";
+import Accounts from "../Accounts/accounts";
 import {Help} from "../Help/help";
 import {FAQ} from "../FAQ/faq";
 import {AboutUS} from "../Aboutus/aboutus";
 import {Admin} from "../Admin/admin";
-
+import "./bankmenu.css"
 export class BankMenu extends React.Component<any, any>{
 
     constructor(props: any, context: any) {
@@ -21,10 +21,15 @@ export class BankMenu extends React.Component<any, any>{
     render() {
         return(
            <HashRouter>
+           <div className="tab-menu">
+               <nav className="navbar navbar-expand-lg navbar-dark bg-light ">
+
             <TabMenu model={this.props.items} activeIndex={this.state.activeIndex}
                      onTabChange={(e) => this.setState({ activeIndex: e.index })} />
 
-           <Switch>
+               </nav>
+                   </div>
+               <Switch>
                <Route path="/Home" component={Home}/>
                <Route path="/Admin" component={Admin}/>
                <Route path="/Accounts" component={Accounts}/>
