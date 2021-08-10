780f4a6ef3e1286b9714a3e606bd913a162df6f7
eswaribala <parameswaribala@gmail.com>
2021-08-10 15:41:49 +0530

    updated

--- day3/banking-app/src/Login/login.tsx
+++ day3/banking-app/src/Login/login.tsx
@@ -1,7 +1,78 @@
-import React from "react";
-export default function Login(){
+import React from 'react';
+import useForm from "./useForm";
+import validate from "./LoginFormValidationRules";
+import Grid from "@material-ui/core/Grid";
+import {TextField} from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import LockIcon from '@material-ui/icons/Lock';
+//React Hook
+//arrow function
+const Login = () => {
+    const {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+    } = useForm(login, validate);
 
-    return(
-        <h1>Login</h1>
-    )
-}
+    function login() {
+        console.log('No errors, submit callback called!');
+    }
+
+    return (
+        <form onSubmit={handleSubmit} noValidate style={{ backgroundColor: '#F0F0F0' }} className="border border-primary shadow-none p-3 mb-5 bg-light rounded w-275" >
+            <fieldset>
+                <legend>User Login</legend>
+                <Grid container spacing={2}>
+
+                    <Grid item xs={12} sm={12}>
+                        <TextField
+                            id="email"
+                            label="Email"
+                            margin="normal"
+                            fullWidth
+                            name="email"
+                            className={`input ${errors.email && 'is-danger'}`}
+                            onChange={handleChange} value={values.email || ''} required
+                        />
+                        {errors.email && (
+                            <p className="help is-danger">{errors.email}</p>
+                        )}
+                    </Grid>
+
+                    <Grid item xs={12} sm={12}>
+                        <TextField
+                            id="password"
+                            label="Password"
+                            margin="normal"
+                            fullWidth
+                            className={`input ${errors.password && 'is-danger'}`}
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            value={values.password || ''} required
+                        />
+                        {errors.password && (
+                            <p className="help is-danger">{errors.password}</p>
+                        )}
+                    </Grid>
+                </Grid>
+                <div className="d-flex justify-content-center mt-5">
+                    <Fab color="primary" aria-label="add"  type="submit" style={{marginRight:50}} >
+                        <LockOpenIcon />
+                    </Fab>
+
+                    <Fab color="secondary" aria-label="add"  type="reset" >
+                        <LockIcon />
+                    </Fab>
+                </div>
+            </fieldset>
+
+
+        </form>
+
+    );
+};
+
+export default Login;
